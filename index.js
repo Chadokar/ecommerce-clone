33e36bdaf531a18d3bfdc1f70b0a1a8bb59e01f4
Shubham Chadokar <shubhamchadokar36@gmail.com>
2023-12-22 12:17:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("Connected to database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
+
+app.use(cors());
+
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+const server = app.listen(8000, () => {
+  console.log("Server listening on port 8000");
+});
