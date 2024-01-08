7b32678f683aae54546b339aac1c40b46f7d4d93
Shubham Chadokar <shubhamchadokar36@gmail.com>
2024-01-09 00:15:32 +0530

    one shop one user

--- controllers/shopcontroller.js
+++ controllers/shopcontroller.js
@@ -1,11 +1,17 @@
 const Shop = require("../models/ShopModels");
+const User = require("../models/UsersModels");
 
 module.exports.createshop = async (req, res) => {
   const { shopname, address, email, pin, userId } = req.body;
+  const user = req.user;
+  if (user?.shopId) {
+    return res.status(401).json({ error: "shop already exist for this user" });
+  }
   const data = { shopname, address, email, pin, userId };
   console.log("shop : ", data);
   try {
     const shop = await Shop.create(data);
+    await User.findByIdAndUpdate(userId, { shopId: shop._id });
     console.log("shop2 : ", shop);
     res.status(201).json({ shop });
   } catch (error) {
